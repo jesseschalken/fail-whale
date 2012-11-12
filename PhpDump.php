fb6e86b4111c0250ccf968a6cde5a08afca0b0a5
Jesse Schalken <me@jesseschalken.com>
2012-11-13 10:03:03 +1100

    Fixed UTF-8 detection on older versions of PHP with a buggy mb_check_encoding

--- PhpDump.php
+++ PhpDump.php
@@ -248,7 +248,7 @@ final class PhpDump
 	{
 		$escaped = '';
 
-		if ( mb_check_encoding( $string, 'UTF-8' ) )
+		if ( self::stringIsUtf8( $string ) )
 		{
 			$strlen = mb_strlen( $string, 'UTF-8' );
 
@@ -505,4 +505,63 @@ final class PhpDump
 
 		return $lines;
 	}
+
+	/**
+	 * From: http://stackoverflow.com/questions/1473441/check-to-see-if-a-string-is-encoded-as-utf-8/1473770#1473770
+	 */
+	private static function stringIsUtf8( $str )
+	{
+		if ( PHP_VERSION_ID > 50408 )
+			return mb_check_encoding( $str, 'UTF-8' );
+
+		# get length, for utf8 this means bytes and not characters
+		$length = strlen( $str );
+
+		# we need to check each byte in the string
+		for ( $i = 0; $i < $length; $i++ )
+		{
+			# get the byte code 0-255 of the i-th byte
+			$c = ord( $str[$i] );
+
+			# utf8 characters can take 1-6 bytes, how much
+			# exactly is decoded in the first character if
+			# it has a character code >= 128 (highest bit set).
+			# For all <= 127 the ASCII is the same as UTF8.
+			# The number of bytes per character is stored in
+			# the highest bits of the first byte of the UTF8
+			# character. The bit pattern that must be matched
+			# for the different length are shown as comment.
+			#
+			# So $n will hold the number of additional characters
+
+			if ( $c < 0x80 )
+				$n = 0; # 0bbbbbbb
+			elseif ( ( $c & 0xE0 ) == 0xC0 )
+				$n = 1; # 110bbbbb
+			elseif ( ( $c & 0xF0 ) == 0xE0 )
+				$n = 2; # 1110bbbb
+			elseif ( ( $c & 0xF8 ) == 0xF0 )
+				$n = 3; # 11110bbb
+			elseif ( ( $c & 0xFC ) == 0xF8 )
+				$n = 4; # 111110bb
+			elseif ( ( $c & 0xFE ) == 0xFC )
+				$n = 5; # 1111110b
+			else
+				return false; # Does not match any model
+
+			# the code now checks the following additional bytes
+			# First in the if checks that the byte is really inside the
+			# string and running over the string end.
+			# The second just check that the highest two bits of all
+			# additional bytes are always 1 and 0 (hexadecimal 0x80)
+			# which is a requirement for all additional UTF-8 bytes
+
+			for ( $j = 0; $j < $n; $j++ ) # n bytes matching 10bbbbbb follow ?
+				if ( ( ++$i == $length ) || ( ( ord( $str[$i] ) & 0xC0 ) != 0x80 ) )
+					return false;
+		}
+
+		return true;
+	}
 }
+
