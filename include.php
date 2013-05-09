14ade05f8ba5992890e4583fd921e68ed0b286f6
Jeffery Fernandez <jeffery@fernandez.net.au>
2013-05-09 14:43:12 +1000

    CORE-181: Fix the require of Error handling pretty printer so that it works independant of autoloading

--- include.php
+++ include.php
@@ -1,12 +1,13 @@
 <?php
 
-require_once dirname( __FILE__ ) . "/pretty printer/PrettyPrinter.php";
-require_once dirname( __FILE__ ) . "/pretty printer/ArrayPrettyPrinter.php";
-require_once dirname( __FILE__ ) . "/pretty printer/ObjectPrettyPrinter.php";
-require_once dirname( __FILE__ ) . "/pretty printer/ExceptionPrettyPrinter.php";
-require_once dirname( __FILE__ ) . "/pretty printer/String.php";
-require_once dirname( __FILE__ ) . "/pretty printer/Settings.php";
-require_once dirname( __FILE__ ) . "/pretty printer/Lines.php";
-require_once dirname( __FILE__ ) . "/pretty printer/Table.php";
-require_once dirname( __FILE__ ) . "/ErrorHandler.php";
-require_once dirname( __FILE__ ) . "/php_constants.php";
+$dirName = dirname( __FILE__ );
+require_once $dirName . "/pretty printer/PrettyPrinter.php";
+require_once $dirName . "/pretty printer/ArrayPrettyPrinter.php";
+require_once $dirName . "/pretty printer/ObjectPrettyPrinter.php";
+require_once $dirName . "/pretty printer/ExceptionPrettyPrinter.php";
+require_once $dirName . "/pretty printer/String.php";
+require_once $dirName . "/pretty printer/Settings.php";
+require_once $dirName . "/pretty printer/Lines.php";
+require_once $dirName . "/pretty printer/Table.php";
+require_once $dirName . "/ErrorHandler.php";
+require_once $dirName . "/php_constants.php";
