e92b1bfe8078492ca9404a6c6d351721e8b967fe
Jesse Schalken <me@jesseschalken.com>
2013-11-30 12:47:46 +1100

    Move PrettyPrinter\Settings namespace into one file

--- src/PrettyPrinter/Setting.php
+++ src/PrettyPrinter/Setting.php
@@ -22,3 +22,37 @@ namespace PrettyPrinter
 		}
 	}
 }
+
+namespace PrettyPrinter\Settings
+{
+	use PrettyPrinter\Setting;
+
+	class Bool extends Setting
+	{
+		function set( $v )
+		{
+			return parent::set( (bool) $v );
+		}
+
+		function yes() { return $this->set( true ); }
+
+		function no() { return $this->set( false ); }
+	}
+
+	class Number extends Setting
+	{
+		function set( $v )
+		{
+			return parent::set( $v === INF || $v === -INF ? $v : (int) $v );
+		}
+	}
+
+	class String extends Setting
+	{
+		function set( $v )
+		{
+			return parent::set( "$v" );
+		}
+	}
+}
+
