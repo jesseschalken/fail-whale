a1e0315857be9b981511561a466a580bd2fd635d
Jesse Schalken <me@jesseschalken.com>
2013-11-30 13:17:23 +1100

    Consolidate ExceptionInfo classes into one file

--- src/PrettyPrinter/ExceptionInfo.php
+++ src/PrettyPrinter/ExceptionInfo.php
@@ -79,4 +79,48 @@ namespace PrettyPrinter
 			return $globals;
 		}
 	}
+
+	interface HasExceptionInfo
+	{
+		/**
+		 * @return ExceptionInfo
+		 */
+		function info();
+	}
+
+	class ExceptionExceptionInfo extends ExceptionInfo
+	{
+		private $e, $localVariables, $stackTrace;
+
+		function __construct( \Exception $e, array $localVariables = null, array $stackTrace = null )
+		{
+			$this->e              = $e;
+			$this->localVariables = $localVariables;
+			$this->stackTrace     = $stackTrace;
+		}
+
+		function message() { return $this->e->getMessage(); }
+
+		function code() { return $this->e->getCode(); }
+
+		function file() { return $this->e->getFile(); }
+
+		function line() { return $this->e->getLine(); }
+
+		function previous()
+		{
+			$previous = $this->e->getPrevious();
+
+			return isset( $previous ) ? ExceptionInfo::fromException( $previous ) : null;
+		}
+
+		function localVariables() { return $this->localVariables; }
+
+		function stackTrace() { return isset( $this->stackTrace ) ? $this->stackTrace : $this->e->getTrace(); }
+
+		function exceptionClassName()
+		{
+			return get_class( $this->e );
+		}
+	}
 }
