b25a369e5d25050004848d273c51e56e361462fe
Jesse Schalken <me@jesseschalken.com>
2013-11-30 12:49:52 +1100

    Move PrettyPrinter\Test namespace into one file

--- src/PrettyPrinter/PrettyPrinterTest.php
+++ src/PrettyPrinter/PrettyPrinterTest.php
@@ -289,3 +289,112 @@ s
 		}
 	}
 }
+
+namespace PrettyPrinter\Test
+{
+	use PrettyPrinter\ExceptionInfo;
+	use PrettyPrinter\Reflection\ClassStaticProperty;
+	use PrettyPrinter\Reflection\FunctionStaticVariable;
+	use PrettyPrinter\Reflection\GlobalVariable;
+	use PrettyPrinter\Reflection\MethodStaticVariable;
+	use PrettyPrinter\Reflection\Variable;
+	use PrettyPrinter\TypeHandlers;
+	use PrettyPrinter\Utils\Ref;
+
+	class DummyClass1
+	{
+		public static $publicStatic1;
+		private static /** @noinspection PhpUnusedPrivateFieldInspection */
+				$privateStatic1;
+		protected static $protectedStatic1;
+		public $public1;
+		private /** @noinspection PhpUnusedPrivateFieldInspection */
+				$private1;
+		protected $protected1;
+	}
+
+	class DummyClass2 extends DummyClass1
+	{
+		public static $publicStatic2;
+		private static /** @noinspection PhpUnusedPrivateFieldInspection */
+				$privateStatic2;
+		protected static $protectedStatic2;
+		public $public2;
+		private /** @noinspection PhpUnusedPrivateFieldInspection */
+				$private2;
+		protected $protected2;
+	}
+
+	class MockException extends ExceptionInfo
+	{
+		function message()
+		{
+			return <<<'s'
+This is a dummy exception message.
+
+lololool
+s;
+		}
+
+		function code()
+		{
+			return 'Dummy exception code';
+		}
+
+		function file()
+		{
+			return '/the/path/to/muh/file';
+		}
+
+		function line()
+		{
+			return 9000;
+		}
+
+		function previous()
+		{
+			return null;
+		}
+
+		function localVariables()
+		{
+			return array(
+				'lol' => 8,
+				'foo' => 'bar',
+			);
+		}
+
+		function stackTrace()
+		{
+			return array(
+				array(
+					'object'   => new DummyClass1,
+					'class'    => 'AClass',
+					'args'     => array( new DummyClass2 ),
+					'type'     => '->',
+					'function' => 'aFunction',
+					'file'     => '/path/to/muh/file',
+					'line'     => 1928,
+				),
+			);
+		}
+
+		function globalVariables()
+		{
+			return array(
+				new ClassStaticProperty( 'BlahClass', 'private', 'blahProperty', Ref::create() ),
+				new FunctionStaticVariable( 'BlahAnotherClass', 'public', Ref::create() ),
+				new GlobalVariable( 'lol global', Ref::create() ),
+				new MethodStaticVariable( 'BlahYetAnotherClass', 'blahMethod', 'lolStatic', Ref::create() ),
+				new Variable( 'blahVariable', Ref::create() ),
+			);
+		}
+
+		function exceptionClassName()
+		{
+			return 'MuhMockException';
+		}
+	}
+}
+
+
