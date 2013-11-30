0bd14dc501d597b4a9d99f3603f1188e09623034
Jesse Schalken <me@jesseschalken.com>
2013-11-30 12:42:31 +1100

    Move PrettyPrinter\Reflection namespace into one file

---
+++ src/PrettyPrinter/Reflection.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace PrettyPrinter\Reflection
+{
+	use PrettyPrinter\Utils\Text;
+	use PrettyPrinter\TypeHandler;
+
+	class ClassStaticProperty extends Variable
+	{
+		private $class, $access;
+
+		function __construct( $class, $access, $name, &$value )
+		{
+			$this->class  = $class;
+			$this->access = $access;
+
+			parent::__construct( $name, $value );
+		}
+
+		function prefix()
+		{
+			return new Text( "$this->access static $this->class::" );
+		}
+	}
+
+	class FunctionStaticVariable extends Variable
+	{
+		private $function;
+
+		function __construct( $function, $name, &$value )
+		{
+			$this->function = $function;
+
+			parent::__construct( $name, $value );
+		}
+
+		function prefix()
+		{
+			return new Text( "function $this->function()::static " );
+		}
+	}
+
+	class GlobalVariable extends Variable
+	{
+		function isSuperGlobal()
+		{
+			return in_array( $this->name(), array( '_POST',
+			                                       '_GET',
+			                                       '_SESSION',
+			                                       '_COOKIE',
+			                                       '_FILES',
+			                                       '_REQUEST',
+			                                       '_ENV',
+			                                       '_SERVER' ), true );
+		}
+
+		function prefix()
+		{
+			return new Text( $this->isSuperGlobal() ? '' : 'global ' );
+		}
+	}
+
+	class MethodStaticVariable extends Variable
+	{
+		private $class, $method;
+
+		function __construct( $class, $method, $name, &$value )
+		{
+			$this->class  = $class;
+			$this->method = $method;
+
+			parent::__construct( $name, $value );
+		}
+
+		function prefix()
+		{
+			return new Text( "function $this->class::$this->method()::static " );
+		}
+	}
+
+	class Variable
+	{
+		private $name, $value;
+
+		function __construct( $name, &$value )
+		{
+			$this->name  = $name;
+			$this->value =& $value;
+		}
+
+		function &value()
+		{
+			return $this->value;
+		}
+
+		function name()
+		{
+			return $this->name;
+		}
+
+		function prefix()
+		{
+			return new Text;
+		}
+
+		function prettyPrint( TypeHandler $any )
+		{
+			return $this->prefix()->appendLines( $any->prettyPrintVariable( $this->name ) );
+		}
+	}
+}
+
