c6d3e883df1abec96c294c07c3ac98c931c5c61f
Jesse Schalken <me@jesseschalken.com>
2015-07-19 17:23:10 +1000

    Add Value::toInlineHTML()

--- src/Value.php
+++ src/Value.php
@@ -2,7 +2,37 @@
 
 namespace FailWhale;
 
-class Value {
+final class Value {
+    /**
+     * Convert multiple values into HTML
+     *
+     * @param self[] $values
+     * @return string
+     */
+    static function manyToHTML(array $values) {
+        $html = '';
+        $hasJS = false;
+        foreach ($values as $value) {
+            $html .= $value->toInlineHTML(!$hasJS);
+            $hasJS = true;
+        }
+
+        ob_start();
+
+        ?>
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <title>fail whale PHP explorer</title>
+            </head>
+            <body><?= $html ?></body>
+        </html>
+        <?php
+
+        return ob_get_clean();
+    }
+
     static function introspect($value, IntrospectionSettings $settings = null) {
         $i = new Introspection($settings);
 
@@ -38,34 +68,45 @@ class Value {
         $this->root = $root;
     }
 
+    /**
+     * Converts the value to a complete HTML document
+     * @return string
+     */
     function toHTML() {
-        $scriptJs = file_get_contents(__DIR__ . '/../web/script.js');
-        $jsonHtml = htmlspecialchars($this->toJSON(), ENT_COMPAT, 'UTF-8');
+        return self::manyToHTML(array($this));
+    }
 
+    /**
+     * Converts the value into HTML suitable for concatenating/embedding in other HTML.
+     *
+     * @param bool $includeJS Whether to include the FailWhale javascript library. It is safe to include multiple
+     *                        times, but the output will be bloated. You can also include web/script.js manually.
+     * @return string
+     */
+    function toInlineHTML($includeJS = true) {
         ob_start();
-        ?>
-        <!DOCTYPE html>
-        <html lang="en">
-            <head>
-                <meta charset="utf-8">
-                <title>fail whale PHP explorer</title>
-                <script>
-                    <?= $scriptJs ?>
-                </script>
-                <script type="text/javascript">
-                    document.addEventListener('DOMContentLoaded', function () {
-                        var json = document.getElementById('the-json');
-                        var body = document.getElementsByTagName('body')[0];
 
-                        body.innerHTML = '';
-                        body.appendChild(FailWhale.renderJSON(json.textContent, document));
-                    });
-                </script>
-            </head>
-            <body>
-                <pre id="the-json"><?= $jsonHtml ?></pre>
-            </body>
-        </html>
+        if ($includeJS) {
+            ?>
+            <script>
+                <?= file_get_contents(__DIR__ . '/../web/script.js') ?>
+            </script>
+            <?php
+        }
+
+        $jsonHtml = htmlspecialchars($this->toJSON(), ENT_COMPAT, 'UTF-8');
+        $uniqueId = 'fail-whale-' . mt_rand();
+
+        ?>
+        <pre id="<?= $uniqueId ?>"><?= $jsonHtml ?></pre>
+        <script type="text/javascript">
+            document.addEventListener('DOMContentLoaded', function () {
+                var json = document.getElementById('<?= $uniqueId ?>');
+                var html = FailWhale.renderJSON(json.textContent, document);
+
+                json.parentNode.replaceChild(html, json);
+            });
+        </script>
         <?php
 
         return ob_get_clean();
