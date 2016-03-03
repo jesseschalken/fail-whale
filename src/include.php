d38243049f74ca7ecc88000b16d83517dc1f8074
Jesse Schalken <me@jesseschalken.com>
2016-03-03 23:55:16 +1100

    Improve autoloader

--- src/include.php
+++ src/include.php
@@ -1,14 +1,13 @@
 <?php
 
-spl_autoload_register(function ($class) {
-    $path = __DIR__;
-    foreach (explode('\\', $class) as $part) {
-        $path .= DIRECTORY_SEPARATOR . $part;
-        if (file_exists("$path.php")) {
-            require_once "$path.php";
-        } else if (!file_exists($path)) {
+spl_autoload_register(function ($cls) {
+    for ($cls = explode('\\', $cls); $cls; array_pop($cls)) {
+        $sep = DIRECTORY_SEPARATOR;
+        $php = __DIR__ . $sep . join($sep, $cls) . '.php';
+        if (file_exists($php)) {
+            /** @noinspection PhpIncludeInspection */
+            require_once $php;
             break;
         }
     }
 });
-
